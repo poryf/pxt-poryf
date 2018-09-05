f237b9046dbce5b22fb2cf455e9973d794336eb6
zhengzhengxiaogege <39449547+zhengzhengxiaogege@users.noreply.github.com>
2018-09-05 16:27:47 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,47 +1,15 @@
-
-//% color="#808080" weight=23 icon="\uf11c"
-namespace pbit_传感器 {
-    
-    export enum enVoice {
-        //% blockId="Voice" block="有声音"
-        Voice = 0,
-        //% blockId="NoVoice" block="无声音"
-        NoVoice= 1
-           }
-    export enum enFire {
-        //% blockId="Fire" block="有火焰"
-        Fire = 0,
-        //% blockId="NoFire" block="无火焰"
-        NoFire = 1
-           }
-    
-     /*声音传感器*/
-    //% blockId=mbit_Voiced_Sensor block="声音传感器 管脚 %pin|状态 %value"
-    //% weight=4
-    //% blockGap=8
-    //% color="#C814B8"
-           export function Voice_Sensor(pin: DigitalPin, value: enVoice): boolean {
-        pins.setPull(pin, PinPullMode.PullUp);
-        if (pins.digitalReadPin(pin) == value) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-           /*火焰传感器*/
-    //% blockId=mbit_Fire_Sensor block="火焰传感器 管脚 %pin|状态 %value"
-    //% weight=4
-    //% blockGap=8
-    //% color="#C814B8"
-           export function Fire_Sensor(pin: DigitalPin, value: enFire): boolean {
-        pins.setPull(pin, PinPullMode.PullUp);
-        if (pins.digitalReadPin(pin) == value) {
-            return true;
-        }
-        else {
-            return false;
-        }
+//%blockId=c_SR04 block="超声波传感器"
+//%color=#5C2D91 weight=100 icon="\uf205"
+namespace SR04 {
+    //%blockId=c_SR04_getDistance block="获取距离(cm) Trig引脚%c_trigPin|Echo引脚%c_echoPin"
+    export function c_SR04_getDistance(c_trigPin: DigitalPin, c_echoPin: DigitalPin): number {
+        pins.setPull(c_trigPin, PinPullMode.PullNone);
+        pins.digitalWritePin(c_trigPin, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(c_trigPin, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(c_trigPin, 0);
+        let d = pins.pulseIn(c_echoPin, PulseValue.High, 2000000);
+        return d / 58;
     }
 }
-    
