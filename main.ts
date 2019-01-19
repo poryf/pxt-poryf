4366270095bebbd5ee624d4faa1ec22b506df286
poryf <42965673+poryf@users.noreply.github.com>
2019-01-19 17:43:48 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -620,3 +620,25 @@ namespace OLED12864显示屏 {
         _ZOOM = 1
     }
 }
+
+//% weight=20 color=#87CEEB icon="8"
+namespace 传感器 {
+    //% blockId=mbit_ultrasonic block="超声波传感器|Trig %Trig|Echo %Echo"
+    //% color="#87CEEB"
+    //% weight=100
+    //% blockGap=10
+    export function Ultrasonic(Trig: DigitalPin, Echo: DigitalPin): number {
+
+        // send pulse
+        pins.setPull(Trig, PinPullMode.PullNone);
+        pins.digitalWritePin(Trig, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(Trig, 1);
+        control.waitMicros(15);
+        pins.digitalWritePin(Trig, 0);
+
+        // read pulse
+        let d = pins.pulseIn(Echo, PulseValue.High, 23200);
+        return Math.floor(d / 58);
+    }
+}
